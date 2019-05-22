a72b39f7f0a2472a0c40d4f5ad181b40733fc33c
Ashley Muller <serenehopes@gmail.com>
2019-05-22 04:51:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  array.join("") if array.legnth == 1
+  array.join("") if array.length == 1
   if array.length >= 2 
     array << "and " + array.pop()
     array.length == 2 ? array.join(" ") : array.join(", ")

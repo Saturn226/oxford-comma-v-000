9569ba351b76489b1b6898e87123c345a16ee484
Ashley Muller <serenehopes@gmail.com>
2019-05-22 04:49:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,11 @@ def oxford_comma(array)
   if array.length == 2 
     array << "and " + array.pop()
   end
+  
+  if array.length > 2
+    array << "and " + array.pop()
+    array.join(", ")
+  end
   array.join(" ")
   
 end
